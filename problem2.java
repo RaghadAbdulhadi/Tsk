b3e2e2a3b487e35be2e406266287688f7dd5331c
RaghadAbdulhadi1 <raghadabdulhadi1999@gmail.com>
2023-05-26 13:40:09 +0300

    modify pb2

--- problem2.java
+++ problem2.java
@@ -2,7 +2,7 @@ public class MaximumNumber {
     public static int findMaximumInteger(int[] numbers) {
         int max = numbers[0];
 
-        for (int i = 1; i < numbers.length; i++) {
+        for (int i = 5; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
             }
